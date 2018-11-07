1cfc67d6402ae114f5c5e95165f5b176eac14e67
Wesley Gauntt <wesleygauntt92@gmail.com>
2018-11-06 21:46:46 -0800

    Moved key into config

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,6 +1,7 @@
 class TripController < ApplicationController
 	def index
-		response = HTTParty.get("http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y")
+		key = Rails.application.config.bart_api_key
+		response = HTTParty.get("http://api.bart.gov/api/stn.aspx?cmd=stns&key=" + key + "&json=y")
 		@stations = JSON.parse(response.body)["root"]["stations"]["station"]
 	end
 end
