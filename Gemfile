5670cefbad8a383114853455b0a7dae3db8ed337
Wesley Gauntt <wesleygauntt92@gmail.com>
2018-11-06 21:19:58 -0800

    Added request to get stations

--- Gemfile
+++ Gemfile
@@ -41,6 +41,10 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # bootstrap
 gem 'bootstrap', '~> 4.0.0'
 
+gem 'pry-byebug'
+
+gem 'httparty'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,5 +1,6 @@
 class TripController < ApplicationController
 	def index
-
+		response = HTTParty.get("http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y")
+		@stations = JSON.parse(response.body)["root"]["stations"]["station"]
 	end
 end
