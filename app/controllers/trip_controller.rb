01fd92874722f928b24aa0af4faf9d0c3d705075
Wesley Gauntt <wesleygauntt92@gmail.com>
2018-11-20 21:16:00 -0800

    Added better response formatting

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -11,6 +11,10 @@ class TripController < ApplicationController
 		response = HTTParty.get("http://api.bart.gov/api/sched.aspx?cmd=depart&orig="+params[:starting_station]+"&dest="+params[:ending_station]+"&date=now&key=" + key + "&b=2&a=2&l=1&json=y")
 		@trips = JSON.parse(response.body)["root"]["schedule"]["request"]["trip"]
 
+		@fares = @trips.first["fares"]["fare"]
+		@origin = @trips.first["@origin"]
+		@destination = @trips.first["@destination"]
+
 		pp @trips
 	end
 end
